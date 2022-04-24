8940814c56dba4c8aaa0bad7a27cf083d8c1b789
ahmed nasser <ahmednas197@gmail.com>
2022-04-24 23:04:30 +0200

    add body-parser for handling uploaded iamges

--- server/app.js
+++ server/app.js
@@ -2,18 +2,21 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const recipeRouter = require('./routes/recipes');
+const bodyParser = require('body-parser');
 require("dotenv").config();
 
+const port = process.env.PORT || 5001;
 
 const app = express();
-const port = process.env.PORT || 5001;
+
+
+//30mb is the max image size you can upload
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 
 // to connect my server side with client side 
 app.use(cors());
-app.use(express.json());
-
 app.use('/recipes', recipeRouter);
-
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true },
     (err) => {
@@ -21,6 +24,4 @@ mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnified
         else console.log("mongodb is connected");
     }
 );
-
-
 app.listen(port, () => console.log(`The server is up and running on port ${port}`));
