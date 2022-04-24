8884153000ffadbeaa0b778d9cb7e3eac35e0853
ahmed nasser <ahmednas197@gmail.com>
2022-04-24 23:23:37 +0200

    add component structure at the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,30 @@
-import './App.css';
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import Recipes from './Components/Recipes/Recipes';
+import Form from './Components/Form/Form';
+import img1 from './images/food.png';
+import useStyles from './styles';
+const App = () => {
+  const classes = useStyles();
 
-function App() {
   return (
-    <div className="App">
-      hello world
-    </div>
+    < Container maxWidth="lg" >
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">Recipe App</Typography>
+        <img className={classes.image} src={img1} alt="recipes" height="100" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justifyContent="space-between" alignItems="stretch" spacing={4}>
+            <Grid item xs={12} sm={7}>
+              <Recipes />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container >
   );
 }
 
---
+++ client/src/Components/Recipes/Recipes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Recipe from './Recipe/Recipe'
+
+export default function Recipes() {
+    return (
+        <>
+            <h1><Recipe /></h1>
+            <h1><Recipe /></h1>
+        </>
+    )
+}
